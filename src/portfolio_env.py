3dd3e4be305e617d376f22fe004010e3aad64e01
Daniel Fudge <daniel.fudge1977@gmail.com>
2020-03-26 02:11:35 -0400

    A good start on the RL environment.

--- src/portfolio_env.py
+++ src/portfolio_env.py
@@ -5,82 +5,71 @@ import gym
 import gym.spaces
 from pprint import pprint
 import numpy as np
-from utils import *
 import csv
-from config import *
+
+EPS = 1e-8
 
 
 class PortfolioEnv(gym.Env):
-    """
-    An environment for financial portfolio management.
-    Financial portfolio management is the process of constant redistribution of a fund into different
-    financial products.
-    Based on [Jiang 2017](https://arxiv.org/abs/1706.10059)
+    """ This class creates the financial market environment that the Agent interact with.
+
+    It extends the OpenAI Gym environment https://gym.openai.com/.
     """
 
-    def __init__(self,
-                 steps=730,  # 2 years
-                 trading_cost=0.0025,
-                 time_cost=0.00,
-                 window_length=7,
-                 start_idx=0,
-                 sample_start_date=None
-                 ):
-        """
-        An environment for financial portfolio management.
-        Params:
-            steps - steps in episode
-            scale - scale data and each episode (except return)
-            augment - fraction to randomly shift data by
-            trading_cost - cost of trade as a fraction
-            time_cost - cost of holding as a fraction
-            window_length - how many past observations to return
-            start_idx - The number of days from '2012-08-13' of the dataset
-            sample_start_date - The start date sampling from the history
+    def __init__(self, steps=730, trading_cost=0.0025, time_cost=0.00, window_length=7, start_idx=0,
+                 sample_start_date=None):
+        """An environment for financial portfolio management.
+
+        Args:
+            steps (int):  Steps in episode.
+            trading_cost (float):  Cost of trade as a fraction.
+            time_cost (float):  Cost of holding as a fraction.
+            window_length (int):  How many past observations to return.
+            start_idx (int):  The number of days from '2012-08-13' of the dataset.
+            sample_start_date (None | str)  The sampling start date , e.g. '2012-08-13', random if `None`.
         """
 
-        datafile = DATA_DIR
-        history, abbreviation = read_stock_history(filepath=datafile)
+        # Read the stock data
+        root = os.path.split(os.path.dirname(__file__))[0]
+        prices = pd.read_pickle(os.path.join(root, 'stock-data-clean.pkl'))
+        open_close = np.dstack((prices.values[:-1].T, prices.values[1:].T))
+        tickers = data.columns.tolist()
+        n_tickers = len(tickers)
+        self.dates = prices.index.tolist()
+
+        # Read the signals
+        signals = pd.read_pickle(os.path.join(root, 'signals.pkl'))
+        n_signals = signals.shape[1]
+
         self.window_length = window_length
-        self.num_stocks = history.shape[0]
+        self.num_stocks = n_tickers
         self.start_idx = start_idx
         self.csv_file = CSV_DIR
 
-        self.src = DataGenerator(history=history,
-                                 abbreviation=abbreviation,
-                                 steps=steps,
-                                 window_length=window_length,
-                                 start_idx=start_idx,
-                                 start_date=sample_start_date)
-        self.sim = PortfolioSim(
-            asset_names=abbreviation,
-            trading_cost=trading_cost,
-            time_cost=time_cost,
-            steps=steps)
+        self.src = DataGenerator(prices=open_close, tickers=tickers, max_steps=steps, window_length=window_length,
+                                 start_idx=start_idx, start_date=sample_start_date, signals=signals)
+        self.sim = PortfolioSim(tickers=tickers, trading_cost=trading_cost, time_cost=time_cost, max_steps=steps)
 
-        # openai gym attributes
-        # action will be the portfolio weights [cash_bias,w1,w2...] where wn are [0, 1] for each asset
-        self.action_space = gym.spaces.Box(
-            0, 1, shape=(len(self.src.asset_names) + 1,), dtype=np.float32)
+        # Define the action space as the portfolio weights [cash_bias,w1,w2...] where wn are [0, 1] for each asset
+        self.action_space = gym.spaces.Box(low=0, high=1, shape=(n_tickers + 1,), dtype=np.float32)
 
-        # get the observation space from the data min and max
-        self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(len(abbreviation), window_length,
-                                                                                 history.shape[-1] - 1), dtype=np.float32)
+        # Define the observation space, which are the signals
+        self.observation_space = gym.spaces.Box(low=-1, high=1, shape=(n_signals, window_length), dtype=np.float32)
 
+    # -----------------------------------------------------------------------------------
     def step(self, action):
-        return self._step(action)
+        """Step the environment.
 
-    def _step(self, action):
+        Args:
+            action (np.array):  The desired portfolio weights [w0...].
+
+        Returns:
+            state
+            reward
+            bool:  Indicates if the simulation is complete.
+            dict:  Debugging information.
         """
-        Step the env.
-        Actions should be portfolio [w0...]
-        - Where wn is a portfolio weight from 0 to 1. The first is cash_bias
-        - cn is the portfolio conversion weights see PortioSim._step for description
-        """
-        np.testing.assert_almost_equal(
-            action.shape,
-            (len(self.sim.asset_names) + 1,)
-        )
+        np.testing.assert_almost_equal(action.shape, (len(self.sim.asset_names) + 1,))
 
         # normalise just in case
         weights = np.clip(action, 0, 1)
@@ -88,10 +77,9 @@ class PortfolioEnv(gym.Env):
         weights[0] += np.clip(1 - weights.sum(), 0, 1)  # so if weights are all zeros we normalise to [1,0...]
 
         assert ((action >= 0) * (action <= 1)).all(), 'all action values should be between 0 and 1. Not %s' % action
-        np.testing.assert_almost_equal(
-            np.sum(weights), 1.0, 3, err_msg='weights should sum to 1. action="%s"' % weights)
+        np.testing.assert_almost_equal(np.sum(weights), 1.0, 3, err_msg='weights must sum to 1. action="%s"' % weights)
 
-        observation, done1, ground_truth_obs = self.src._step()
+        state, done1, ground_truth_obs = self.src.take_step()
 
         # concatenate observation with ones
         cash_observation = np.ones((1, self.window_length, observation.shape[2]))
@@ -101,15 +89,14 @@ class PortfolioEnv(gym.Env):
         ground_truth_obs = np.concatenate((cash_ground_truth, ground_truth_obs), axis=0)
 
         # relative price vector of last observation day (close/open)
-        close_price_vector = observation_concat[:, -1, 3]
+        close_price_vector = observation_concat[:, -1, 1]
         open_price_vector = observation_concat[:, -1, 0]
         y1 = close_price_vector / open_price_vector
         reward, info, done2 = self.sim._step(weights, y1)
 
         # calculate return for buy and hold a bit of each asset
         info['market_value'] = np.cumprod([inf["return"] for inf in self.infos + [info]])[-1]
-        # add dates
-        info['date'] = index_to_date(self.start_idx + self.src.idx + self.src.step)
+        info['date'] = self.dates(self.start_idx + self.src.idx + self.src.step)
         info['steps'] = self.src.step
         info['next_obs'] = ground_truth_obs
 
@@ -122,7 +109,7 @@ class PortfolioEnv(gym.Env):
                 dict_writer = csv.DictWriter(f, keys)
                 dict_writer.writeheader()
                 dict_writer.writerows(self.infos)
-        return observation, reward, done1 or done2, info
+        return state, reward, done1 or done2, info
 
     def reset(self):
         return self._reset()
@@ -131,11 +118,8 @@ class PortfolioEnv(gym.Env):
         self.infos = []
         self.sim.reset()
         observation, ground_truth_obs = self.src.reset()
-        cash_observation = np.ones((1, self.window_length, observation.shape[2]))
-        observation_concat = np.concatenate((cash_observation, observation), axis=0)
         cash_ground_truth = np.ones((1, 1, ground_truth_obs.shape[2]))
         ground_truth_obs = np.concatenate((cash_ground_truth, ground_truth_obs), axis=0)
-        info = {}
         info['next_obs'] = ground_truth_obs
         return observation
 
@@ -143,78 +127,78 @@ class PortfolioEnv(gym.Env):
 class DataGenerator(object):
     """Acts as data provider for each new episode."""
 
-    def __init__(self, history, abbreviation, steps=730, window_length=50, start_idx=0, start_date=None):
+    def __init__(self, prices, signals, tickers, max_steps=730, window_length=50, start_idx=0, start_date=None):
         """
-
         Args:
-            history: (num_stocks, timestamp, 5) open, high, low, close, volume
-            abbreviation: a list of length num_stocks with assets name
-            steps: the total number of steps to simulate, default is 2 years
-            window_length: observation window, must be less than 50
-            start_date: the date to start. Default is None and random pick one.
-                        It should be a string e.g. '2012-08-13'
+            prices (np.array): [n_tickers, n_days, [open, close]] Stock price history.
+            signals (np.array): [n_signals, n_days]  Signals to drive decisions.
+            tickers (list):  Tickers of the stocks.
+            max_steps (int):  The total number of steps to simulate, default is 2 years
+            window_length (int):  Observation window, must be less than 50
+            start_date (None | str):  The date to start, e.g. '2012-08-13'. Random if `None`.
         """
-        assert history.shape[0] == len(abbreviation), 'Number of stock is not consistent'
-        import copy
+        assert prices.shape[0] == len(tickers), 'Number of stock is not consistent'
 
-        self.steps = steps + 1
-        self.window_length = window_length
+        self.tickers = tickers
+        self.prices = prices.copy()
+        self.idx = 0
+        self.signals = signals.copy()
         self.start_idx = start_idx
         self.start_date = start_date
+        self.step = 0
+        self.max_steps = max_steps + 1
+        self.window_length = window_length
 
-        # make immutable class
-        self._data = history.copy()  # all data
-        self.asset_names = copy.copy(abbreviation)
+        # Internal parameters
+        self._prices = prices.copy()
+        self._signals = signals.copy()
 
-    def _step(self):
-        # get price matrix (open, close, high, low) from history.
+    def take_step(self):
+        """Generate the current and next state of the environment."""
         self.step += 1
-        obs = self.data[:, self.step:self.step + self.window_length, :].copy()
+        state = self.signals[:, self.step:self.step + self.window_length]
+        next_state = self.prices[:, self.step + self.window_length:self.step + self.window_length + 1]
 
-        # used for compute optimal action and sanity check
-        ground_truth_obs = self.data[:, self.step + self.window_length:self.step + self.window_length + 1, :].copy()
-
-        done = self.step >= self.steps
-        return obs, done, ground_truth_obs
+        done = self.step >= self.max_steps
+        return state, done, next_state
 
     def reset(self):
+        """Resets the environment."""
+
         self.step = 0
-        # get data for this episode, each episode might be different.
         if self.start_date is None:
-            self.idx = np.random.randint(
-                low=self.window_length, high=self._data.shape[1] - self.steps)
+            self.idx = np.random.randint(low=self.window_length, high=self.prices.shape[1] - self.max_steps)
         else:
             # compute index corresponding to start_date for repeatable sequence
-            self.idx = date_to_index(self.start_date) - self.start_idx
-            assert self.idx >= self.window_length and self.idx <= self._data.shape[1] - self.steps, \
-                'Invalid start date, must be window_length day after start date and simulation steps day before end date'
-        data = self._data[:, self.idx - self.window_length:self.idx + self.steps + 1, :4]
-        self.data = data
-        return self.data[:, self.step:self.step + self.window_length, :].copy(), \
-               self.data[:, self.step + self.window_length:self.step + self.window_length + 1, :].copy()
+            self.idx = np.where(self.dates == self.start_date)[0][0] - self.start_idx
+            error_text = 'Must have Windows length before start date and simulation steps after.'
+            assert self.window_length <= self.idx <= self._prices.shape[1] - self.max_steps, error_text
+
+        self.signals = self._signals[:, self.idx - self.window_length:self.idx + self.max_steps + 1, :4]
+
+        state = self.signals[:, self.step:self.step + self.window_length, :].copy()
+        next_state = self.signals[:, self.step + self.window_length:self.step + self.window_length + 1, :].copy()
+        return state, next_state
 
 
 class PortfolioSim(object):
-    """
-    Portfolio management sim.
-    Params:
-    - cost e.g. 0.0025 is max in Poliniex
-    Based of [Jiang 2017](https://arxiv.org/abs/1706.10059)
-    """
+    """ The environment simulator.  Determines the rewards and new state based on the actions and price history."""
 
-    def __init__(self, asset_names=list(), steps=730, trading_cost=0.0025, time_cost=0.0):
-        self.asset_names = asset_names
+    def __init__(self, tickers=(), max_steps=730, trading_cost=0.0025, time_cost=0.0):
+        self.tickers = tickers
         self.cost = trading_cost
         self.time_cost = time_cost
-        self.steps = steps
+        self.max_steps = max_steps
+        self.infos = []
+        self.w0 = np.array([1.0] + [0.0] * len(self.tickers))
+        self.p0 = 1.0
 
     def _step(self, w1, y1):
-        """
-        Step.
-        w1 - new action of portfolio weights - e.g. [0.1,0.9,0.0]
-        y1 - price relative vector also called return
-            e.g. [1.0, 0.9, 1.1]
-        Numbered equations are from https://arxiv.org/abs/1706.10059
+        """Step in the environment. Numbered equations are from https://arxiv.org/abs/1706.10059
+
+        Args:
+            w1 (np.array):   New portfolio weights, e.g. [0.1,0.9,0.0].
+            y1 (np.array):   Price relative vector also called return, e.g. [1.0, 0.9, 1.1].
         """
         assert w1.shape == y1.shape, 'w1 and y1 must have the same shape'
         assert y1[0] == 1.0, 'y1[0] must be 1'
@@ -232,7 +216,7 @@ class PortfolioSim(object):
 
         rho1 = p1 / p0 - 1  # rate of returns
         r1 = np.log((p1 + EPS) / (p0 + EPS))  # log rate of return
-        reward = r1 / self.steps * 1000.  # (22) average logarithmic accumulated return
+        reward = r1 / self.max_steps * 1000.  # (22) average logarithmic accumulated return
         # remember for next step
         self.w0 = w1
         self.p0 = p1
@@ -240,20 +224,12 @@ class PortfolioSim(object):
         # if we run out of money, we're done (losing all the money)
         done = bool(p1 == 0)
 
-        info = {
-            "reward": reward,
-            "log_return": r1,
-            "portfolio_value": p1,
-            "return": y1.mean(),
-            "rate_of_return": rho1,
-            "weights_mean": w1.mean(),
-            "weights_std": w1.std(),
-            "cost": mu1,
-        }
+        info = {"reward": reward, "log_return": r1, "portfolio_value": p1, "return": y1.mean(), "rate_of_return": rho1,
+                "weights_mean": w1.mean(), "weights_std": w1.std(), "cost": mu1}
         self.infos.append(info)
         return reward, info, done
 
     def reset(self):
         self.infos = []
-        self.w0 = np.array([1.0] + [0.0] * len(self.asset_names))
+        self.w0 = np.array([1.0] + [0.0] * len(self.tickers))
         self.p0 = 1.0
